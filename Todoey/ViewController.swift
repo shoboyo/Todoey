09941b028eb1e68381156e52b653ab9cc21730c4
Raileanu Andrei <shoboyo@raileanus-mac.local>
2018-11-17 11:14:17 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Raileanu Razvan Andrei on 17/11/2018.
+//  Copyright © 2018 MetaLanguage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
